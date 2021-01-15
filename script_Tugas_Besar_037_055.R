e56bd8add4d43d8c4fbc5c17dc5b7a67965a720b
Kennedi Nadeak <57614533+Kennedi27@users.noreply.github.com>
2021-01-15 16:39:23 +0700

    Update script_Tugas_Besar_037_055.R

--- script_Tugas_Besar_037_055.R
+++ script_Tugas_Besar_037_055.R
@@ -1,30 +1,37 @@
 #Set Area Kerja
-setwd("D:/Documents/WD/TB-DataMining")
-#Data Set yang akan digunakan
-dataset <- read.csv("Immunotherapy.csv", sep = ",")
-#Aktifkan library C50 dan printr 
+setwd("Documents/WD/TB-DataMining")
+#Load Data Set yang akan digunakan
+dataset3 <- read.csv("Immunotherapy.csv", sep = ",")
+#Install Library
+#Jika Sudah terinstall abaikan saja
+install.packages("C50") 
+install.packages("printr")
+#Jalankan/Panggil library C50 dan printr
 library(C50)
 library(printr)
 #Membuat model menggunakan C5.0
-model <- C5.0(Result_of_Treatment ~., data=dataset)
+model3 <- C5.0(Result_of_Treatment ~., data=dataset3)
 #jika model error, lanjutkan langkah berikut
 #cek class/tipe dari kolom Result_of_Trearment
-class(dataset$Result_of_Treatment)
+class(dataset3$Result_of_Treatment)
 #mengubah tipe class ke factor dan jalankan kembali model <- C5.0(Result_of_Treatment ~., data=dataset)
-dataset$Result_of_Treatment <- as.factor(dataset$Result_of_Treatment)
+dataset3$Result_of_Treatment <- as.factor(dataset3$Result_of_Treatment)
+model3 <- C5.0(Result_of_Treatment ~., data=dataset3)
 #melihat hasil model
-model
-summary(model)
+model3
+summary(model3)
 #menampilkam gambar/pohon model
-plot(model)
-#membuat dataset
-datatesting <- dataset[,1:7]
+plot(model3)
+#membuat data Testing dari dataset3
+datatesting3 <- dataset3[,1:7]
 #prediksi
-predictions <- predict(model, datatesting)
+predictions3 <- predict(model3, datatesting3)
+head(predictions3)
 #membandingkan hasil prediksi dari datatesting dengan dataset
-table(predictions, dataset$Result_of_Treatment)
-
-#Memngetahui rule model
-rulemodel <- C5.0(Result_of_Treatment ~., data = dataset, rules = TRUE)
+table(predictions3, dataset3$Result_of_Treatment)
+#Melihat tingkat akurasi
+mean(predictions3  == dataset3$Result_of_Treatment)
+#Memngetahui rule dari model yang di buat
+rulemodel <- C5.0(Result_of_Treatment ~., data = dataset3, rules = TRUE)
 rulemodel
 summary(rulemodel)
