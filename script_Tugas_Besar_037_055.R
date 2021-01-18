e8117e10e51ad3f68c967a27e34d04ae78b6be5a
Jusriyani Sirait <72424897+jusriyani@users.noreply.github.com>
2021-01-18 21:57:55 +0700

    Update TugasBesar_with_C50

--- script_Tugas_Besar_037_055.R
+++ script_Tugas_Besar_037_055.R
@@ -1,37 +1,50 @@
 #Set Area Kerja
-setwd("Documents/WD/TB-DataMining")
+setwd("C:/wd/TB")
+getwd()
 #Load Data Set yang akan digunakan
 dataset3 <- read.csv("Immunotherapy.csv", sep = ",")
+
 #Install Library
 #Jika Sudah terinstall abaikan saja
 install.packages("C50") 
 install.packages("printr")
+
 #Jalankan/Panggil library C50 dan printr
 library(C50)
 library(printr)
-#Membuat model menggunakan C5.0
-model3 <- C5.0(Result_of_Treatment ~., data=dataset3)
-#jika model error, lanjutkan langkah berikut
-#cek class/tipe dari kolom Result_of_Trearment
+library(dplyr)
+#cek Struktur dataset
+str(dataset3)
+
+#Cek class/tipe dari kolom Result_of_Treatment
 class(dataset3$Result_of_Treatment)
-#mengubah tipe class ke factor dan jalankan kembali model <- C5.0(Result_of_Treatment ~., data=dataset)
+
+#Mengubah tipe class ke factor
 dataset3$Result_of_Treatment <- as.factor(dataset3$Result_of_Treatment)
-model3 <- C5.0(Result_of_Treatment ~., data=dataset3)
-#melihat hasil model
+
+#Membagi Data Training dan Testing dengan persentase 80%
+set.seed(123)
+training.sample3 <- sample(nrow(dataset3)*0.8)
+train.data3 <- dataset3[training.sample3,]
+test.data3 <- dataset3[-training.sample3,]
+
+#Membuat Model dan Melihat isi model
+set.seed(123)
+model3 <- C5.0(Result_of_Treatment ~., data=train.data3, control = C5.0Control(minCases = 0))
 model3
 summary(model3)
-#menampilkam gambar/pohon model
+
+#Menampilkam gambar/pohon model
 plot(model3)
-#membuat data Testing dari dataset3
-datatesting3 <- dataset3[,1:7]
-#prediksi
+
+#Prediksi data test 
+datatesting3 <- test.data3[,1:7]
 predictions3 <- predict(model3, datatesting3)
+
+#Melihat beberapa hasil prediksi
 head(predictions3)
-#membandingkan hasil prediksi dari datatesting dengan dataset
-table(predictions3, dataset3$Result_of_Treatment)
+#Membandingkan hasil prediksi dari datatesting dengan dataset
+table(predictions3, test.data3$Result_of_Treatment)
+
 #Melihat tingkat akurasi
-mean(predictions3  == dataset3$Result_of_Treatment)
-#Memngetahui rule dari model yang di buat
-rulemodel <- C5.0(Result_of_Treatment ~., data = dataset3, rules = TRUE)
-rulemodel
-summary(rulemodel)
+mean(predictions3  == test.data3$Result_of_Treatment)
